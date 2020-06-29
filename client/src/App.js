7cdf4850c37849562d6af8da0776a9c33a3ee10c
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-29 20:10:27 +1000

    fetching jobs from front end, useEffect and state, parsing JSON

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
 import Jobs from './Jobs'
 
 function App() {
+  const [jobs, setjobs] = useState([])
+
+  const JOB_API_URL = 'http://localhost:3001/jobs'
 
   const mockJobs = [
     {title:'SWE', company:'Google',},
@@ -11,11 +14,25 @@ function App() {
     {title:'USA', company:'ADT',},
     {title:'CA', company:'VEXA',},
     {title:'TR', company:'dREE',},
-]
+  ]
+  
+  const fetchJobs = async (updatejobs) =>{
+    const res = await fetch(JOB_API_URL)
+    let json = await res.json()
+
+    console.log({json})
+
+    //updateState
+    updatejobs(json)
+  }
+
+  useEffect(() => {
+    const jobs = fetchJobs(setjobs)
+  }, [])
 
   return (
     <div className="App">
-      <Jobs jobs={mockJobs} />
+      <Jobs jobs={jobs} />
     </div>
   );
 }
